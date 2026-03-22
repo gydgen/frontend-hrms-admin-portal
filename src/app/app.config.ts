cb49f0c2fc33d45daff5b8bc6dca4283d95644a9
takyifidelis <takyifidelis@gmail.com>
2026-03-22 19:57:12 +0000

    feat: add new features and configurations including NgRx store, PrimeNG button, and theme presets; create new files for auth guard, employee routes, app state, and environment settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,60 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import {
+  ApplicationConfig,
+  inject,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { isActive, IsActiveMatchOptions, provideRouter, Router, withViewTransitions } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import { provideStore } from '@ngrx/store';
+
+const BlueAura = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{blue.50}',
+      100: '{blue.100}',
+      200: '{blue.200}',
+      300: '{blue.300}',
+      400: '{blue.400}',
+      500: '{blue.500}',
+      600: '{blue.600}',
+      700: '{blue.700}',
+      800: '{blue.800}',
+      900: '{blue.900}',
+      950: '{blue.950}',
+    },
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes, withViewTransitions({
+        onViewTransitionCreated: ({ transition }) => {
+            const router = inject(Router);
+            const targetUrl = router.currentNavigation()!.finalUrl!;
+            // Skip transition if only fragment or query params change
+            const config: IsActiveMatchOptions = {
+                paths: 'exact',
+                matrixParams: 'exact',
+                fragment: 'ignored',
+                queryParams: 'ignored',
+            };
+            const isTargetRouteCurrent = isActive(targetUrl, router, config);
+            if (isTargetRouteCurrent()) {
+                transition.skipTransition();
+            }
+        },
+    })),
+    providePrimeNG({
+        theme: {
+            preset: BlueAura,
+        },
+    }),
+    provideStore()
+],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
