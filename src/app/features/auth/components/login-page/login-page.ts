02a08417d26b6cb60d514b371258859336606cce
takyifidelis <takyifidelis@gmail.com>
2026-03-24 07:28:15 +0000

    feat: redesign login page with new layout and styles; remove old login component and its tests

--- src/app/features/auth/components/login-page/login-page.ts
+++ src/app/features/auth/components/login-page/login-page.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+  ],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
-export class LoginPage {
-
-}
+export class LoginPage {}
